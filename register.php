832fbe9f8eb30a7b0a0dae77068798bffcb5177b
Richard Jonáš <jonas.richard11@gmail.com>
2023-12-03 22:01:30 +0100

    JS is working!!!

--- register.php
+++ register.php
@@ -4,24 +4,46 @@ require 'includes/form_handlers/register_handler.php';
 require 'includes/form_handlers/login_handler.php';
 ?>
 
+
 <html>
 <head>
-	<title>Welcome to Social!</title>
-	<link rel="stylesheet" href="assets/css/register_style.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+	<title>Welcome to Swirlfeed!</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/register_style.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="assets/js/register.js"></script>
 </head>
 <body>
 
+	<?php  
+
+	if(isset($_POST['register_button'])) {
+		echo '
+		<script>
+
+		$(document).ready(function() {
+			$("#first").hide();
+			$("#second").show();
+		});
+
+		</script>
+
+		';
+	}
+
+
+	?>
+
 	<div class="wrapper">
+
 		<div class="login_box">
 
 			<div class="login_header">
 				<h1>Social!</h1>
-				<p>Login or sign up below.</p>
+				Login or sign up below!
 			</div>
-			
+			<br>
 			<div id="first">
+
 				<form action="register.php" method="POST">
 					<input type="email" name="log_email" placeholder="Email Address" value="<?php 
 					if(isset($_SESSION['log_email'])) {
@@ -34,27 +56,13 @@ require 'includes/form_handlers/login_handler.php';
 					<?php if(in_array("Email or password was incorrect<br>", $error_array)) echo  "Email or password was incorrect<br>"; ?>
 					<input type="submit" name="login_button" value="Login">
 					<br>
-					<a href="# id="signup" class="signup">Need and account? Register here!</a>
+					<a href="#" id="signup" class="signup">Need and account? Register here!</a>
 
 				</form>
+
 			</div>
 
 			<div id="second">
-				<form action="register.php" method="POST">
-					<input type="email" name="log_email" placeholder="Email Address" value="<?php 
-					if(isset($_SESSION['log_email'])) {
-						echo $_SESSION['log_email'];
-					} 
-					?>" required>
-					<br>
-					<input type="password" name="log_password" placeholder="Password">
-					<br>
-					<?php if(in_array("Email or password was incorrect<br>", $error_array)) echo  "Email or password was incorrect<br>"; ?>
-					<input type="submit" name="login_button" value="Login">
-					<br>
-					<a href="# id="signup" class="signup">Need and account? Register here!</a>
-
-				</form>
 
 				<form action="register.php" method="POST">
 					<input type="text" name="reg_fname" placeholder="First Name" value="<?php 
@@ -66,6 +74,8 @@ require 'includes/form_handlers/login_handler.php';
 					<?php if(in_array("Your first name must be between 2 and 25 characters<br>", $error_array)) echo "Your first name must be between 2 and 25 characters<br>"; ?>
 					
 					
+
+
 					<input type="text" name="reg_lname" placeholder="Last Name" value="<?php 
 					if(isset($_SESSION['reg_lname'])) {
 						echo $_SESSION['reg_lname'];
@@ -103,19 +113,16 @@ require 'includes/form_handlers/login_handler.php';
 
 					<input type="submit" name="register_button" value="Register">
 					<br>
-				
 
-					<?php if(in_array("<span style='color: #14C800;'>You're all set! Goahead and login!</span><br>", $error_array)) echo "<span style='color: #14C800;'>You're all set! Go ahead and login!</span><br>"; ?>
-					<a href="# id="signin" class="signin">Already heve and account? Sigh in here!</a>
-
+					<?php if(in_array("<span style='color: #14C800;'>You're all set! Go ahead and login!</span><br>", $error_array)) echo "<span style='color: #14C800;'>You're all set! Go ahead and login!</span><br>"; ?>
+					<a href="#" id="signin" class="signin">Already have an account? Sign in here!</a>
 				</form>
-			</div>		
+			</div>
 
-			
-
-			</form>
 		</div>
+
 	</div>
+
 
 </body>
 </html>
