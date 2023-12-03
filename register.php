f97bac34cc4e9360df29ab128f1021ae194d4bee
Richard Jonáš <jonas.richard11@gmail.com>
2023-12-03 22:05:24 +0100

    WIP

--- register.php
+++ register.php
@@ -7,7 +7,7 @@ require 'includes/form_handlers/login_handler.php';
 
 <html>
 <head>
-	<title>Welcome to Swirlfeed!</title>
+	<title>Welcome to Social!</title>
 	<link rel="stylesheet" type="text/css" href="assets/css/register_style.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="assets/js/register.js"></script>
