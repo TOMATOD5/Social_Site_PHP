7a0bfd1762261b9eee158a8cfe8204c331b141b0
Richard Jonáš <jonas.richard11@gmail.com>
2023-12-04 18:18:07 +0100

    bootstrap rdy

--- includes/form_handlers/header.php
+++ includes/form_handlers/header.php
@@ -15,8 +15,20 @@ else {
 <html>
 <head>
 	<title>Welcome to Social!</title>
+
+    <!-- JavaScript -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.js"></script>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
+    <div class="top_bar">
+        <div class="logo">
+            <a href="index.php">Social!</a>
 
-    radom
+        </div>
+
+    </div>
