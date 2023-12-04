eea2310145ab069383e41e294d0f2351bdf13886
Richard Jonáš <jonas.richard11@gmail.com>
2023-12-04 18:27:08 +0100

    Navbar wip

--- includes/form_handlers/header.php
+++ includes/form_handlers/header.php
@@ -21,6 +21,7 @@ else {
     <script src="assets/js/bootstrap.js"></script>
 
     <!-- CSS -->
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="assets/css/bootstrap.css">
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
@@ -30,5 +31,32 @@ else {
             <a href="index.php">Social!</a>
 
         </div>
+
+        <nav>
+			<a href="<?php echo $userLoggedIn; ?>">
+				<?php echo $user['first_name']; ?>
+			</a>
+			<a href="index.php">
+				<i class="fa fa-home fa-lg"></i>
+			</a>
+			<a href="#">
+				<i class="fa fa-envelope fa-lg"></i>
+			</a>
+			<a href="#">
+				<i class="fa fa-bell fa-lg"></i>
+			</a>
+			<a href="#">
+				<i class="fa fa-users fa-lg"></i>
+			</a>
+			<a href="#">
+				<i class="fa fa-cog fa-lg"></i>
+			</a>
+			<a href="includes/handlers/logout.php">
+				<i class="fa fa-sign-out fa-lg"></i>
+			</a>
+
+
+
+		</nav>
 
     </div>
